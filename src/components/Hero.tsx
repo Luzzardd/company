0359c7d884322e7002f6d832b7152fd636f6abc2
Luzzardd <mnaufalhasyim112@gmail.com>
2024-12-13 01:00:04 +0700

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
             <Image 
                 className="mx-auto rounded-xl py-8 md:py-0 mt-10" 
                 src={logo}
-                alt="About Me" 
+                alt="About M" 
                 width={450} 
                 height={300} 
             />
